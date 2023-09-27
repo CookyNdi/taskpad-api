5cbf26e1f5f3f56d42e2f612ee24a6edad000052
CookyNdi <yandikun145@gmail.com>
2023-09-27 14:06:11 +0700

    (feat) - Projects : adding endpoint edit name, description, priority, deadline

--- src/controllers/Projects.controller.ts
+++ src/controllers/Projects.controller.ts
@@ -65,6 +65,74 @@ export const createProject = async (req: CustomRequest, res: Response, next: Nex
   }
 }
 
+export const updateProjectName = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { name }: { name: string } = req.body
+    const project: null | { id: string } = await prisma.projects.findUnique({ where: { id: req.projectId } })
+    if (project == null) {
+      return res.status(404).json({ msg: 'Project not found' })
+    }
+    await prisma.projects.update({
+      where: { id: project.id },
+      data: { name }
+    })
+    return res.status(200).json({ msg: 'Project updated successfully' })
+  } catch (error: any) {
+    return res.status(500).json({ msg: error.message })
+  }
+}
+
+export const updateProjectDescription = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { description }: { description: string } = req.body
+    const project: null | { id: string } = await prisma.projects.findUnique({ where: { id: req.projectId } })
+    if (project == null) {
+      return res.status(404).json({ msg: 'Project not found' })
+    }
+    await prisma.projects.update({
+      where: { id: project.id },
+      data: { description }
+    })
+    return res.status(200).json({ msg: 'Project updated successfully' })
+  } catch (error: any) {
+    return res.status(500).json({ msg: error.message })
+  }
+}
+
+export const updateProjectPriority = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { priority }: { priority: string } = req.body
+    const project: null | { id: string } = await prisma.projects.findUnique({ where: { id: req.projectId } })
+    if (project == null) {
+      return res.status(404).json({ msg: 'Project not found' })
+    }
+    await prisma.projects.update({
+      where: { id: project.id },
+      data: { priority }
+    })
+    return res.status(200).json({ msg: 'Project updated successfully' })
+  } catch (error: any) {
+    return res.status(500).json({ msg: error.message })
+  }
+}
+
+export const updateProjectDeadline = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
+  try {
+    const { deadline }: { deadline: string } = req.body
+    const project: null | { id: string } = await prisma.projects.findUnique({ where: { id: req.projectId } })
+    if (project == null) {
+      return res.status(404).json({ msg: 'Project not found' })
+    }
+    await prisma.projects.update({
+      where: { id: project.id },
+      data: { deadline }
+    })
+    return res.status(200).json({ msg: 'Project updated successfully' })
+  } catch (error: any) {
+    return res.status(500).json({ msg: error.message })
+  }
+}
+
 export const deleteProject = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
   try {
     const project = await prisma.projects.delete({
--- src/routes/Projects.router.ts
+++ src/routes/Projects.router.ts
@@ -1,7 +1,16 @@
 import express from 'express'
 
 import { authentication, authorization } from '../helpers/middleware'
-import { createProject, deleteProject, getProjectById, getProjects } from '../controllers/Projects.controller'
+import {
+  createProject,
+  deleteProject,
+  getProjectById,
+  getProjects,
+  updateProjectDeadline,
+  updateProjectDescription,
+  updateProjectName,
+  updateProjectPriority
+} from '../controllers/Projects.controller'
 
 const route = express.Router()
 
@@ -17,11 +26,36 @@ route.get(
   authorization as unknown as any,
   getProjectById as unknown as any
 )
-route.post(
-  '/api/project',
+route.post('/api/project', authentication as unknown as any, createProject as unknown as any)
+
+route.patch(
+  '/api/project/edit/name',
   authentication as unknown as any,
-  createProject as unknown as any
+  authorization as unknown as any,
+  updateProjectName as unknown as any
 )
+
+route.patch(
+  '/api/project/edit/description',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateProjectDescription as unknown as any
+)
+
+route.patch(
+  '/api/project/edit/priority',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateProjectPriority as unknown as any
+)
+
+route.patch(
+  '/api/project/edit/deadline',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateProjectDeadline as unknown as any
+)
+
 route.delete(
   '/api/project',
   authentication as unknown as any,
