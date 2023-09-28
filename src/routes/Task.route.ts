a374217b7607d8bd9bf3cd93aeb0657f6bb96f7b
CookyNdi <yandikun145@gmail.com>
2023-09-28 14:23:31 +0700

    (feat) - Tasks : creating endpoint CRUD task

---
+++ src/routes/Task.route.ts
@@ -0,0 +1,33 @@
+import express from 'express'
+
+import { authentication, authorization } from '../helpers/middleware'
+import { createTask, getTaskById, getTasks, updateTaskStatus, updateTaskTitle } from '../controllers/Task.controllers'
+
+const route = express.Router()
+
+route.get('/api/tasks', authentication as unknown as any, authorization as unknown as any, getTasks as unknown as any)
+route.get(
+  '/api/task/:id',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  getTaskById as unknown as any
+)
+route.post('/api/task', authentication as unknown as any, authorization as unknown as any, createTask as unknown as any)
+route.patch(
+  '/api/task/update/title/:id',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateTaskTitle as unknown as any
+)
+route.patch(
+  '/api/task/update/status/:id',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateTaskStatus as unknown as any
+)
+route.delete(
+  '/api/task/delete',
+  authentication as unknown as any,
+  authorization as unknown as any,
+  updateTaskStatus as unknown as any
+)
