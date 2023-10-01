fba31afc44d3ab94a2e48f4a7527b512625f6be3
CookyNdi <yandikun145@gmail.com>
2023-10-01 11:32:36 +0700

    (docs) : setup swagger

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import express, { type Application, type Request, type Response, type NextFunction } from 'express'
 import fileUpload from 'express-fileupload'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 import userRoute from './routes/Users.route'
 import projectRoute from './routes/Projects.router'
 import taskRoute from './routes/Task.route'
 dotenv.config()
+
+const options = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Taskpad API',
+      version: '1.0.0'
+    }
+  },
+  apis: ['./src/docs/*.ts']
+}
 
 const app: Application = express()
 const PORT: string = process.env.PORT ?? '5000'
 
 app.use(fileUpload())
 app.use(express.static('./public'))
+app.use(cors())
 app.use(
   express.urlencoded({
     extended: true
@@ -19,6 +34,8 @@ app.use(
 )
 app.use(express.json())
 app.use(cookieParser())
+const specs = swaggerJSDoc(options)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }))
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ status: '200' })
